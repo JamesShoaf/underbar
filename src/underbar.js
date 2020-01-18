2e7a3fcd597b0e473678b5950fe173c0f8f64ed5
James <james.s.shoaf@gmail.com>
2020-01-17 18:04:18 -0800

    implemented _.each

--- src/underbar.js
+++ src/underbar.js
@@ -48,8 +48,14 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
-    for (i = 0; i < collection.length; i++){
-      iterator(collection[i]);
+    if (Array.isArray(collection)){
+      for (var i = 0; i < collection.length; i++){
+        iterator(collection[i], i, collection);
+      }
+    } else if (typeof collection === 'object') {
+      for (var key in collection){
+        iterator(collection[key], key, collection);
+      }
     }
   };
 
